b7c3637c74510763d480bab6730736218a09886a
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2020-12-19 09:15:31 +0100

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import "./sass/style.scss";
+
+export function App() {
+
+    return (
+        <div className="app">
+            <div className="background">
+
+            </div>
+        </div>
+    );
+}
