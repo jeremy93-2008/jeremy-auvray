7302809e6f352306cb10db60d3b1303e22445d30
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2020-12-20 11:10:31 +0100

    feat: More thing added

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,39 @@
+import * as React from "react";
 import "./sass/style.scss";
 
 export function App() {
+    const photo = require("./images/photo.svg");
+
+    const icons = [
+        require("./images/js.svg"),
+        require("./images/ts.svg"),
+        require("./images/react.svg"),
+        require("./images/electron.svg"),
+        require("./images/nodejs.svg"),
+        require("./images/webpack.svg"),
+        require("./images/rollup.svg"),
+        require("./images/ex.svg"),
+        require("./images/sass.svg"),
+        require("./images/styled.svg"),
+        require("./images/git.svg"),
+        require("./images/vscode.svg"),
+    ];
 
     return (
         <div className="app">
-            <div className="background">
-
+            <div className="wrapper">
+                <div className="jeremy-container">
+                    <img className="photo" src={photo} />
+                    <div className="name">
+                        Jeremy Auvray
+                    </div>
+                    <div className="description">
+                        Javascript Developer
+                    </div>
+                    <div className="qualities">
+                        {icons.map(ico => <div className="icon"><img src={ico} /></div>)}
+                    </div>
+                </div>
             </div>
         </div>
     );
